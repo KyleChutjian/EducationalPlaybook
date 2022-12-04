e800149a6374059e1cc6a4717dcf3f1035da4b90
Samantha Spinner <64045431+sspinner224@users.noreply.github.com>
2022-12-04 17:38:02 -0500

    Update intakeForm.js

--- backend/models/intakeForm.js
+++ backend/models/intakeForm.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const intakeFormSchema = new mongoose.Schema({
-    clientId: {type: User[], required: true},
-    programLeadId: {type: User[], required: true},
-    curriculumId: {type: Curriculum[], required: true},
+    clientId: {type: Object, required: true},
+    programLeadId: {type: Object, required: true},
+    curriculumId: {type: Object, required: true},
     intakeResponse: {type: String[], required: true},
     status: {type: String, required: true},
     needsAssessment: {type: String[][], required: true}
