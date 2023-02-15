bf51b270b158192688649db89bfe690fc03c3f76
sspinner224 <sspinner@quinnipiac.edu>
2023-02-15 12:20:01 -0500

    made navbar a reusable component

--- frontend/src/components/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
-import { NavLink } from "react-router-dom";
 import { loginUser } from '../services/authService';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Card} from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-import ClientNav from '../components/ClientNav';
+import AdminNav from '../components/AdminNav';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 // import { ReactComponent as Logo } from './logo.svg';
 
 import {
@@ -20,54 +18,63 @@ import {
 }
 from 'mdb-react-ui-kit';
 
-function Dashboard() {
+function AdminDashboard() {
 
   
-    return(  
+    return(
+
+      
     <div>
       <div>
-        <ClientNav/>
+       <AdminNav/>  
       </div>
+     
     <div class="container-fluid text-sm-center p-3 bg-light" style={{fontFamily: 'Bitter'}}>
-        <h1> Client Dashboard</h1>
-        <h3> Welcome client!</h3>
+        <h1> Admin Dashboard</h1>
+        <h3> Welcome Tom!</h3>
     </div> 
     <div class="col d-flex justify-content-center">
-    
+
+
     <Container fluid>
      <Card className="text-center mx-auto" style={{ background: '#0098C3', width: '60rem', margin:'5px', color:'whitesmoke', fontFamily: 'Bitter' }}>
       <Card.Body>
-        <Card.Title style={{fontSize:'30px'}}>Intake Forms</Card.Title>
-        <Button variant="light">Submit New</Button>
+        <Card.Title>Pending Forms</Card.Title>
+        <Card.Text>
+        </Card.Text>
+        <Button variant="light">Review</Button>
       </Card.Body>
     </Card>
 
     <Card className="text-center mx-auto" style={{ background: '#6E9A35', width: '60rem', margin:'5px', color:'whitesmoke', fontFamily: 'Bitter'}}>
       <Card.Body>
-        <Card.Title style={{fontSize:'30px'}}>Needs Assessment</Card.Title>
-        <Button variant="light" disabled>Analyze</Button>
+        <Card.Title>Approved Forms</Card.Title>
+        <Card.Text>
+        </Card.Text>
+        <Button variant="light">View</Button>
       </Card.Body>
     </Card>
 
     <Card className="text-center mx-auto" style={{ background: '#d2492a', width: '60rem', margin:'5px', color:'whitesmoke', fontFamily: 'Bitter'}}>
       <Card.Body>
-        <Card.Title style={{fontSize:'30px'}}>Course</Card.Title>
+        <Card.Title>Archived Forms</Card.Title>
         <Card.Text>
         </Card.Text>
-        <Button variant="light" disabled>View</Button>
+        <Button variant="light">View</Button>
       </Card.Body>
     </Card>
 
     <Card className="text-center mx-auto" style={{ background: '#a40084', width: '60rem', margin:'5px', color:'whitesmoke', fontFamily: 'Bitter' }}>
       <Card.Body>
-        <Card.Title style={{fontSize:'30px'}}>Complete Courses</Card.Title>
+        <Card.Title>Success Rate</Card.Title>
         <Card.Text>
         </Card.Text>
-        <Button variant="light" disabled>View</Button>
+        <Button variant="light">View</Button>
       </Card.Body>
     </Card> 
     </Container>
     
+
     </div>
       
     </div>
@@ -75,4 +82,4 @@ function Dashboard() {
   )
 }
 
-export default Dashboard;
+export default AdminDashboard;
