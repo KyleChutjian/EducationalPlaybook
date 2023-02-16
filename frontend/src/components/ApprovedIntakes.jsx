0a229d50d68827367023a1c037f51bb1289b43cc
Sadjell <57645947+Sadjell@users.noreply.github.com>
2023-02-15 21:27:26 -0500

    pending and approved intakes UI

--- frontend/src/components/ApprovedIntakes.jsx
+++ frontend/src/components/ApprovedIntakes.jsx
@@ -6,6 +6,14 @@ import {
   MDBContainer,
   MDBInput,
   MDBCheckbox,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+  MDBTextArea,
   MDBBtn,
   MDBIcon
 }
@@ -14,10 +22,83 @@ from 'mdb-react-ui-kit';
 function ApprovedIntakes() {
 
   
+
+  
     return(
-        <div>
-            <h1>Approved Intakes Screen</h1>
+
+      <div className="intake-wrapper">
+        {/* Navbar */}
+        <header style={{ paddingLeft: 0 }}>
+          <MDBNavbar expand='lg' light bgColor='white'>
+            <MDBContainer fluid>
+              <MDBNavbarToggler
+                aria-controls='navbarExample01'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+              >
+                <MDBIcon fas icon='bars' />
+              </MDBNavbarToggler>
+              <div className='collapse navbar-collapse' id='navbarExample01'>
+                <MDBNavbarNav right className='mb-2 mb-lg-0'>
+                  <MDBNavbarItem active>
+                    <MDBNavbarLink aria-current='page' href='#'>
+                      Sample Navbar
+                    </MDBNavbarLink>
+                  </MDBNavbarItem>
+                  
+                </MDBNavbarNav>
+              </div>
+            </MDBContainer>
+          </MDBNavbar>
+        </header>
+        {/* Jumbotron */}
+        <div className='head-approved p-5 text-center'>
+          <h1 className='mb-3'>Approved Intake Forms</h1>
         </div>
+
+        <div className="intake-body container" style={{paddingTop: "1%"}}>
+          {/* Intake 1 */}
+          <div class="rectangle">
+						<h5 class="d-flex justify-content-center">Intake #001</h5>
+					</div>
+
+          {/* Intake 2 */}
+          <div class="rectangle">
+						<h5 class="d-flex justify-content-center">Intake #002</h5>
+					</div>
+          
+
+          {/* Question 3 */}
+          <div class="rectangle">
+						<h5 class="d-flex justify-content-center">Intake #003</h5>
+					</div>
+
+          
+
+
+
+
+
+
+          <div className="row" style={{paddingTop: "2%", paddingBottom: "5%"}}>
+            <div className="col-sm-12 text-center">
+              <span style={{paddingRight: "1%"}}>
+                <MDBBtn 
+                  id="saveAndCloseIntakeButton" 
+                  className="button-approved"
+                  style={{width: "150px"}}
+                  >View More</MDBBtn>
+              </span>
+              
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      
+       
   )
 }
 
