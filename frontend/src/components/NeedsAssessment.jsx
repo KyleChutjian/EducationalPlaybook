ee20b6e4000cc8e7848b6b36c4b6373eff1700c0
ijcrawford <ijcrawford@quinnipiac.edu>
2023-03-05 20:01:48 -0500

    setup

--- frontend/src/components/NeedsAssessment.jsx
+++ frontend/src/components/NeedsAssessment.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import {Card} from 'react-bootstrap';
+import ClientNav from '../components/ClientNav';
+import { Dropdown, DropdownButton, Button } from 'react-bootstrap';
 import { loginUser } from '../services/authService';
+//other service variables
 import {
   MDBContainer,
   MDBInput,
@@ -14,74 +19,21 @@ from 'mdb-react-ui-kit';
 //later call: localStorage.getItem("currentIntakeId");
 
 function NeedsAssessment() {
+  //constants for needs assessment
+  //const [<varName>,<setterMethod>] = useState(<initial state>);
+
+  useEffect(() => {
+    //get/set functions
+    //get<thing>by<otherthingid> (<thing>.id).then((res) => {<setting function(s)>});
+    
+  }, []);
+
+  //constant buttonClick functions/routes
 
   
-    return(
-        <div>
-          {/* Navbar */}
-      <header style={{ paddingLeft: 0 }}>
-        <ClientNav/>
-      </header>
-
-<table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Focus Area</th>
-      <th scope="col">Desired Future State</th>
-      <th scope="col">Current State</th>
-      <th scope="col">Identified Gap</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">Example</th>
-      <td>To do good</td>
-      <td>We are doing okay.</td>
-      <td>30%</td>
-      <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" style={{fontFamily: 'Bitter', color: black, backgroundColor: white}}>Edit
-</button></td>
-    </tr>
-    <tr>
-      <th scope="row">Example2</th>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" style={{fontFamily: 'Bitter', color: black, backgroundColor: white}}>Edit
-</button></td>
-    </tr>
-    <tr>
-      <th scope="row">Example3</th>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" style={{fontFamily: 'Bitter', color: black, backgroundColor: white}}>Edit
-</button></td>
-    </tr>
-  </tbody>
-</table>
-<div className="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h1 className="modal-title fs-5" id="editModalLabel">Edit Focus</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-      <input type="text" class="form-control" aria-label="Focus Area" />
-      <input type="text" class="form-control" aria-label="Desired Future State" />
-      <input type="text" class="form-control" aria-label="Current State" />
-      <input type="text" class="form-control" aria-label="Identified Gap" />
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-        </div>
-  )
+  return(
+    <div>Needs Assessment Screen</div>
+  );
 }
 
 export default NeedsAssessment;
