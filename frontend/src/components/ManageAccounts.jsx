188781a6e89cbe76a036297fb466d4d2ce60931c
ijcrawford <ijcrawford@quinnipiac.edu>
2023-03-23 07:10:09 -0400

    manageaccounts setup

--- frontend/src/components/ManageAccounts.jsx
+++ frontend/src/components/ManageAccounts.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { loginUser } from '../services/authService';
+import { getCurrentUser } from '../services/authService';
+
 import {
   MDBContainer,
   MDBInput,
@@ -13,7 +15,10 @@ from 'mdb-react-ui-kit';
 
 function ManageAccounts() {
 
-  
+  //get each user from mongo as object? to turn into table items
+  //find where variable for access level is
+  //link bubble to variable, update with save button
+  //start generic, get better after
     return(
         <div>
             <h1>ManageAccounts Screen</h1>
