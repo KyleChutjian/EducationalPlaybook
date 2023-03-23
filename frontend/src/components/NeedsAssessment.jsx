528ca24650aa5574e7fa1b9565623f94db18e269
sspinner224 <sspinner@quinnipiac.edu>
2023-03-22 23:13:27 -0400

    needs assessment functionality

--- frontend/src/components/NeedsAssessment.jsx
+++ frontend/src/components/NeedsAssessment.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { loginUser } from '../services/authService';
@@ -9,13 +9,16 @@ import {Card} from 'react-bootstrap';
 import { Dropdown, DropdownButton, Button } from 'react-bootstrap';
 import { ListGroup } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import { getIntakeByIntakeId, editNeedsAssessmentByIntakeId } from '../services/intakeService';
+
 
 import {
   MDBContainer,
   MDBInput,
   MDBCheckbox,
   MDBBtn,
-  MDBIcon
+  MDBIcon,
+  MDBTextArea
 }
 from 'mdb-react-ui-kit';
 
@@ -23,8 +26,124 @@ function NeedsAssessment() {
 
   const [ adminNavbar, setAdminNavbar ] = useState(false);
 
-  const [ curriculumId, setCurriculumId ] = useState("");
-  const [ curriculumTitle, setCurriculumTitle ] = useState("");
+  
+  const [ intakeTitle, setIntakeTitle ] = useState("");
+  const [ currentIntakeId, setCurrentIntakeId] = useState(localStorage.getItem("currentIntakeId"));
+
+  const [ needsAssessmentData, setNeedsAssessmentData ] = useState("");
+  const [ needsAssessmentCards, setNeedsAssesmentCards ] = useState(<div></div>);
+
+  useEffect(() => {
+
+    console.log("hello");
+    // Permissions
+    const permissionLevel = localStorage.getItem("permission-level");
+    if (permissionLevel === "admin") {
+      setAdminNavbar(true);
+    }
+
+    // Get Curriculum using CurrentIntakeId
+    getIntakeByIntakeId(currentIntakeId).then((intake) => {
+      console.log(intake.data.needsAssessment);
+      setCurrentIntakeId(intake.data._id);
+      setIntakeTitle(`Needs Assessment: ${intake.data.name}`);
+      console.log(typeof intake.data.needsAssessment);
+      console.log(intake.data.needsAssessment[0]);
+      setNeedsAssessmentData(intake.data.needsAssessment);
+      loadNeedsAssessmentCards(intake.data.needsAssessment);
+    });
+      
+  }, [currentIntakeId]);
+
+
+  const loadNeedsAssessmentCards = (cards) => {
+    setNeedsAssesmentCards(
+      cards.map((card, index) => {
+        return(
+          <div className="container" key={card[0]}>
+            <div className="col d-flex justify-content-center">
+              <Card id='card1' className="text-right mx-auto" style={{width: '60rem', margin:'20px', color:'black', fontFamily: 'Bitter' }}>
+                <Card.Header style={{fontSize:'20px'}}> 
+                <Form.Label style={{width:'50%', marginLeft:'25%', textAlign:'center'}}>Focus Area: </Form.Label>
+                <Form.Control style={{width:'50%', marginLeft:'25%', textAlign:'center'}} defaultValue={card[0]} type="focusArea" placeholder='ex. Innovation' name={`focus${index}`} onChange={handleNeedsAssessmentResponseChange}/> </Card.Header>
+                <Card.Body>
+
+                  <div class="card-block">
+                    <div class="row">
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formFutureState">
+                        <Form.Label>Desired Furture State: </Form.Label>
+                        <Form.Control type="futureState" defaultValue={card[1]} name={`future${index}`} onChange={handleNeedsAssessmentResponseChange} placeholder='At least 50% of our developer resources working on new features'/>
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formCurrentState">
+                        <Form.Label>Current State: </Form.Label>
+                        <Form.Control type="currentState" placeholder='Only 10% of our developer resources are working on new features' defaultValue={card[2]} name={`current${index}`} onChange={handleNeedsAssessmentResponseChange}/>
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formCurrentState">
+                        <Form.Label>Identified Gap: </Form.Label>
+                        <Form.Control type="currentState" defaultValue={card[3]} name={`gap${index}`} placeholder='ex. 40%' onChange={handleNeedsAssessmentResponseChange}/>
+                      </Form.Group>
+                    </Form>
+                       
+                    </div>
+                  </div>
+    
+                </Card.Body>
+              </Card>
+                
+          </div>
+          </div>
+        )
+      }
+    ))
+  }
+
+  const handleNeedsAssessmentResponseChange = (e) => {
+    const { name, value} = e.target;
+    let indexString, index;
+    setNeedsAssessmentData((oldData) => {
+      if (name.includes("focus")) {
+        indexString = name.split("focus")[1];
+        index = parseInt(indexString);
+        oldData[index][0] = value;
+      } else if (name.includes("future")) {
+        indexString = name.split("future")[1];
+        index = parseInt(indexString);
+        
+        oldData[index][1] = value;
+      } else if (name.includes("current")) {
+        indexString = name.split("current")[1];
+        index = parseInt(indexString);
+        
+        oldData[index][2] = value;
+      } else if (name.includes("gap")) {
+        indexString = name.split("gap")[1];
+        index = parseInt(indexString);
+        
+        oldData[index][3] = value;
+      } else {
+        console.log("Something went wrong");
+      }
+      return oldData;
+    });
+  }
+
+  const saveChanges = () => {
+    editNeedsAssessmentByIntakeId(currentIntakeId, {newNeedsAssessment: needsAssessmentData})
+  }
+
+  const addNew = (e) => {
+    console.log(needsAssessmentData);
+    var newNeedsAssessment = needsAssessmentData;
+    console.log(typeof needsAssessmentData);
+    newNeedsAssessment = needsAssessmentData.concat([['','','','']]);
+    console.log(newNeedsAssessment);
+    setNeedsAssessmentData(newNeedsAssessment);
+    loadNeedsAssessmentCards(newNeedsAssessment);
+  }
+
   
     return(
         <div>
@@ -34,11 +153,13 @@ function NeedsAssessment() {
 
           {/* Jumbotron */}
           <div className='p-5 text-center' style={{backgroundColor: '#6E9A35', color:'whitesmoke'}}>
-          <h1 className='mb-3' style={{fontFamily: 'Bitter'}}>Needs Assessment {curriculumTitle}</h1>
+          <h1 className='mb-3' style={{fontFamily: 'Bitter'}}>{intakeTitle}</h1>
           </div>
 
+          {needsAssessmentCards}
+
           {/* Assessment Cards */}
-          <div className="col d-flex justify-content-center">
+          {/* <div className="col d-flex justify-content-center">
               <Card id='card1' className="text-right mx-auto" style={{width: '60rem', margin:'20px', color:'black', fontFamily: 'Bitter' }}>
                 <Card.Header style={{fontSize:'20px'}}> 
                 <Form.Label style={{width:'50%', marginLeft:'25%', textAlign:'center'}}>Focus Area: </Form.Label>
@@ -62,8 +183,6 @@ function NeedsAssessment() {
                         <Form.Label>Identified Gap: </Form.Label>
                         <Form.Control type="currentState" placeholder='ex. 40%'/>
                       </Form.Group>
-
-                      <Button style={{backgroundColor: '#6E9A35', justifyContent:'center'}} variant="primary" type="submit">Save</Button>
                     </Form>
                        
                     </div>
@@ -72,11 +191,12 @@ function NeedsAssessment() {
                 </Card.Body>
               </Card>
                 
-          </div>
+          </div> */}
 
                 <div class='row'>
                   <div class='col'>
-                    <Button style={{backgroundColor: '#0098C3', fontFamily: 'Bitter', justifyContent:'center', marginLeft:'48%'}} type="submit">New</Button>
+                    <Button style={{backgroundColor: '#0098C3', fontFamily: 'Bitter', justifyContent:'center', marginLeft:'48%'}} type="submit" onClick={addNew}>New</Button>
+                    <Button style={{backgroundColor: '#6E9A35', justifyContent:'center'}} variant="primary" type="submit" onClick={saveChanges}>Save</Button>
                   </div>   
                 </div>
                 <br/>
