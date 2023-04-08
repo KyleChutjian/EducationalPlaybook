0df0266e5cf0d93c57229e8b250913b2614e71de
ijcrawford <ijcrawford@quinnipiac.edu>
2023-04-08 15:42:14 -0400

    manage accounts

--- frontend/src/components/ManageAccounts.jsx
+++ frontend/src/components/ManageAccounts.jsx
@@ -23,29 +23,81 @@ function ManageAccounts() {
     //
 
     //get all current accounts
-    const users = getAccounts();
+    //const users = getAccounts(); //make into useState
+    const [ users, setUsers ] = useState([]);
+    const [ usersHtml, setUsersHtml ] = useState(<div></div>);
 
     useEffect(() => {
+      //loadUsers(users);
+    }, [users]);
 
-      /* const tableItems = users.map((user) => {
-        <tr>
+    useEffect(() => {
+      getAccounts().then((result) => {
+        setUsers(result.data);
+        loadUsers(result.data);
+      });
+      
+    }, []);
+
+    const loadUsers = (users) => {
+      if (users == [] && typeof(users)) {
+        return <div></div>;
+      }
+      console.log(users);
+      const tableItems = users.map((user,index) => {
+        return (<tr>
           <td>{user.email}</td>
           <td>{user.lastName}, {user.firstName}</td>
-          <td><input className="form-check-input" type="checkbox" value="" id="checkAdmin" defaultChecked={user.isProgramLead} onChange={onChangeUser}/></td>
-          <td><input className="form-check-input" type="checkbox" value="" id="checkAdmin" defaultChecked={user.isAdmin} onChange={onChangeUser}/></td>
-        </tr> 
-      }); */
-
-    }, [])
+          <td><input className="form-check-input" type="checkbox" name={`projectLead${index}`} value="" id="checkProjectLead" defaultChecked={user.isProjectLead} onChange={onChangeUser}/></td>
+          <td><input className="form-check-input" type="checkbox" name={`admin${index}`} value="" id="checkAdmin" defaultChecked={user.isAdmin} onChange={onChangeUser}/></td>
+        </tr> )
+      });
+      setUsersHtml(tableItems);
+    }
 
     const savePermsButton = () => {
-      //update permissions for each user
-      //call ManageAccounts (once for each role, 'output' is T/F)
+      console.log(users);
+      users.forEach((user) => {
+        manageAccounts(user._id,"projectLead",user.isProjectLead);
+        manageAccounts(user._id,"admin",user.isAdmin);
+      })      
     };
   
     const onChangeUser = (e) => {
-      const {name, value, checked} = e.target; //e.target.<attribute> for stuff
+      const {name, value, checked} = e.target; //e.target.<attribute> for stuf
+      console.log(value, checked);
+      //get type of checkbox
+      //get userid/index
+      var indexString, index;
+      if (name.includes("projectLead")) {
+        indexString = name.split("projectLead")[1];
+        index = parseInt(indexString);
+        setUsers((oldUsers) => {
+          console.log(oldUsers[index].isProjectLead);
+          oldUsers[index].isProjectLead = checked;
+          console.log(oldUsers[index].isProjectLead);
+          return oldUsers;
+        })
+      }
+      if (name.includes("admin")) {
+        indexString = name.split("admin")[1];
+        index = parseInt(indexString);
+        setUsers((oldUsers) => {
+          oldUsers[index].isAdmin = checked;
+          return oldUsers;
+        })
+      }
+      
 
+    // console.log(`name: ${name}`);
+    // console.log(`value: ${value}`);
+    // let indexString, index;
+    // setCurriciulumLearningObjectives((oldObjectives) => {
+    //   indexString = name.split("objective")[1];
+    //   index = parseInt(indexString);
+    //   oldObjectives[index] = value;
+    //   return oldObjectives;
+    // });
     }
 
     return(
@@ -61,23 +113,23 @@ function ManageAccounts() {
               <tr>
                 <th scope="col">Email</th>
                 <th scope="col">Employee</th>
-                <th scope="col">Program Lead</th>
+                <th scope="col">Project Lead</th>
                 <th scope="col">Admin</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
+              {usersHtml}
+              {/* <tr>
                 <td scope="row">thomas.nowicki@hhchealth.org</td>
                 <td>Nowicki, Tom</td>
                 <td>
                   <input className="form-check-input" type="checkbox" value="" id="checkProgram" />
                 </td>
                 <td><input className="form-check-input" type="checkbox" value="" id="checkAdmin" defaultChecked={true} onChange={onChangeUser}/></td>
-             </tr>
-    
+             </tr> */}
             </tbody>
           </table>
-          <button onClick="savePermsButton" type="button" class="btn btn-primary" style={{fontFamily: 'Bitter', borderColor: 'white', backgroundColor: '#6E9A35', maxWidth: '200px', width: '30%', marginLeft: '40%', marginRight: '40%'}}>Save</button>
+          <button onClick={savePermsButton} type="button" class="btn btn-primary" style={{fontFamily: 'Bitter', borderColor: 'white', backgroundColor: '#6E9A35', width: '30%', marginLeft: '40%', marginRight: '40%'}}>Save</button>
         </div>
   )
 }
